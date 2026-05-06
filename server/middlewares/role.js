02944a4a50f6906637bfe7f5f7a6080305847a96
sp4chi <herock76@gmail.com>
2026-05-07 02:15:11 +0530

    - added role

---
+++ server/middlewares/role.js
@@ -0,0 +1,8 @@
+export const authorize = (role) => {
+  return (req, res, next) => {
+    if (req.user.role !== role) {
+      return res.status(403).json({ msg: 'Access denied' });
+    }
+    next();
+  };
+};
