8f669c40eb12adefb5e86d3e32700a73df53ded0
sp4chi <herock76@gmail.com>
2026-05-07 02:26:52 +0530

    - added task routes

---
+++ server/routes/taskRoutes.js
@@ -0,0 +1,27 @@
+import { Router } from 'express';
+import Task from '../models/Task';
+import auth from '../middleware/auth';
+
+const router = Router();
+
+// Create task
+router.post('/', auth, async (req, res) => {
+  const task = await Task.create(req.body);
+  res.json(task);
+});
+
+// Get tasks
+router.get('/', auth, async (req, res) => {
+  const tasks = await Task.find().populate('assignedTo');
+  res.json(tasks);
+});
+
+// Update status
+router.patch('/:id', auth, async (req, res) => {
+  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.json(task);
+});
+
+export default router;
