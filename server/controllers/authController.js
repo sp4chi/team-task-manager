27b59900621500c7fdfce3aff30793214885e3cd
sp4chi <herock76@gmail.com>
2026-05-07 02:14:48 +0530

    - added auth controller

---
+++ server/controllers/authController.js
@@ -0,0 +1,45 @@
+import User from '../models/User';
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+
+export const signup = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const hashed = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashed,
+      role,
+    });
+
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ msg: 'Signup failed' });
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) return res.status(400).json({ msg: 'User not found' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid creds' });
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+    );
+
+    res.json({ token, user });
+  } catch (err) {
+    res.status(500).json({ msg: 'Login failed' });
+  }
+};
