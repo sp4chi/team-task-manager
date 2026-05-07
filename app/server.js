7c2d04dbe0f0f707bd74e6ce7337ecdf3628ca0d
sp4chi <herock76@gmail.com>
2026-05-07 08:09:19 +0530

    - cors error

--- app/server.js
+++ app/server.js
@@ -11,15 +11,22 @@ dotenv.config({ path: new URL('.env', import.meta.url) });
 //console.log('MONGO_URI:', process.env.MONGO_URI);
 const app = express();
 
-const corsOptions = {
-  origin: '*',
-  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || '*',
+    credentials: true,
+  }),
+);
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true }));
+// const corsOptions = {
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+// };
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-app.use(express.json());
+// app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions));
 
 mongoose
   .connect(process.env.MONGO_URI)
